e4705bcf384c929bc3fd1e48d3779b72ad0018bb
Abhishek <abhishek.yps@gmail.com>
2023-11-28 22:41:54 +0530

    feature: Question edit

--- src/routes/question.routes.ts
+++ src/routes/question.routes.ts
@@ -1,15 +1,20 @@
 import QuestionController from "@/controller/question.controller";
+import AuthController from "@/controller/user/auth.controller";
+import { validateBody } from "@/utils/library/validate";
+import { questionEditValidation } from "@/validations/question/question";
 import { Router } from "express";
 
 class QuestionRoutes {
   public routes: Router;
   public baseRoute: string;
   private questionController: QuestionController;
+  private authController: AuthController;
 
   constructor() {
     this.routes = Router();
     this.baseRoute = "/question";
     this.questionController = new QuestionController();
+    this.authController = new AuthController();
     this.useRoutes();
   }
 
@@ -22,6 +27,13 @@ class QuestionRoutes {
     this.routes.post(
       "/createMultiple",
       this.questionController.createMultipleQuestions
+    );
+    this.routes.post(
+      "/edit",
+      this.authController.authenticate,
+      questionEditValidation,
+      validateBody,
+      this.questionController.editQuestion
     );
   }
 }
