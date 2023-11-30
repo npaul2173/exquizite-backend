f1db01c00260d740fca925365302064ab564417a
Nabendu Paul <npaul2173@gmail.com>
2023-11-30 09:31:12 +0530

    update: Create permission checked if already exists

--- src/controller/permission.controller.ts
+++ src/controller/permission.controller.ts
@@ -1,6 +1,10 @@
 import { CreatePermission } from "@/models/permission/types";
 import PermissionService from "@/service/permission.service";
-import { getCreateResponse } from "@/utils/helpers/response";
+import {
+  getConflictResponse,
+  getCreateResponse,
+  getInternalServerErrorResponse,
+} from "@/utils/helpers/response";
 import { IReq, IRes } from "@/utils/interfaces/express.interface";
 
 class PermissionController {
@@ -11,13 +15,22 @@ class PermissionController {
 
   createPermission = async (req: IReq, res: IRes) => {
     const inputData = { ...req.body } as CreatePermission;
-    const data = await this.permissionService.saveOne(inputData);
-    const response = getCreateResponse(
-      res,
-      "Permission created successfully",
-      data
-    );
-    return response;
+    const permissionExists = await this.permissionService.find(inputData.name);
+    try {
+      if (permissionExists)
+        return getConflictResponse(res, "Permission already exists");
+      else {
+        const data = await this.permissionService.saveOne(inputData);
+        const response = getCreateResponse(
+          res,
+          "Permission created successfully",
+          data
+        );
+        return response;
+      }
+    } catch (error) {
+      return getInternalServerErrorResponse(res, error);
+    }
   };
 }
 
